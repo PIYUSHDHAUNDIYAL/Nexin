df199d398df67bc42dafd431cdb81462a04cd7b4
PIYUSH <piyushdhaundiyal5@gmail.com>
2026-04-28 22:46:49 +0530

    Move Login import to pages directory

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { Home } from './pages/Home';
 import { Shop } from './pages/Shop';
 import { ProductDetails } from './pages/ProductDetails';
 import { CartDrawer } from './components/CartDrawer';
-import { Login } from './components/Login';
+import { Login } from './pages/Login';
 import { supabase } from './services/supabase';
 
 import { Product, CartItem } from './types';
